c7ab99b2fceb462d01b59a5890f6794a444c4ee8
Marina <musicbymarina@gmail.com>
2018-09-25 03:26:33 +0200

    remove news from menu

--- javascript/index.js
+++ javascript/index.js
@@ -2,43 +2,44 @@
 When I click on Portfolio in the menu, I can see the submenu
 */
 $('#portfolio').on('click', ()=>{
-	$('#portfolio').css({'background-color': 'white',
-	'border-radius': '50px 20px', 'border-left-color': 'grey'});
-	
-	$('#portfolio a').css({'color': 'black', 'font-weight': 'bold'});
-	$('#portfolio-child li > a').css({'color': 'white'});
-	if($(window).width() < 700){
-	$('#portfolio-child').slideToggle();
-	$('#works').toggle();
-	$('#news').toggle();
-	$('#contact').toggle();
-	} else {
-	$('#works-child').hide();
-	$('#portfolio-child').slideToggle();
-	}
 
+	if($(window).width() < 700){
+		$('#portfolio-child').slideToggle();
+		$('#works').toggle();
+		$('#contact').toggle();
+	} else {
+		$('#works-child').hide();
+		$('#portfolio-child').slideToggle();
+	}
+	
+	/*$('#portfolio').css({'background-color': 'white',
+	'border-radius': '50px 20px', 'border-left-color': 'grey'});
+	$('#portfolio-child li > a').css({'color': 'white'});
+	*/
+		$('#portfolio a').css({'color': 'black', 'font-weight': 'bold'});
 });
 
 /*
 When I click on Works in the menu, I can see the submenu
 */
 $('#works').on('click', ()=>{
+	
+	if($(window).width() < 700){
+		$('#works-child').slideToggle();
+		$('#contact').toggle();
+	} else {
+		$('#portfolio-child').hide();
+		$('#works-child').slideToggle();
+	}
+	
 	console.log('the element clicked is: ' + $(this));
+	const worksPrincipal = $('#works a');
+	console.log('Le <a> du works principal a comme color' + worksPrincipal.style.color);
 	/*$('#works').css({'background-color': 'white',
 	'border-radius': '50px 20px', 'border-left-color': 'grey'});
-	
-	$('#works a').css({'color': 'black', 'font-weight': 'bold'});
-	
 	$('#works-child li > a').css({'color': 'white'});
 	*/
-	if($(window).width() < 700){
-	$('#works-child').slideToggle();
-	$('#news').toggle();
-	$('#contact').toggle();
-	} else {
-	$('#portfolio-child').hide();
-	$('#works-child').slideToggle();
-	}
+	$('#works a').css({'color': 'black', 'font-weight': 'bold'});
 });
 
 $('#burger div').on('mouseenter', ()=>{
