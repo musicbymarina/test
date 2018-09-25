ebe9ab27652729835cb81ff05eb25e9d871ded58
Marina <musicbymarina@gmail.com>
2018-09-25 22:02:21 +0200

    fix the pola

--- javascript/images.js
+++ javascript/images.js
@@ -201,13 +201,7 @@ const showNextOrPreviousPicture = (whichOne) => {
 	}
 	else {
 		console.log('ceci etait la derniere toff du slider');
-		if($('.active') === $('#slider li:last-child')) {
-			$('.active figure').css({'box-shadow': '0 1px 1px rgba(0,0,0,0.15)'});
-		} else {
-			$('.active figure').css({'box-shadow': '0 1px 1px rgba(0,0,0,0.15), 0 10px 0 -5px #eee, 0 10px 1px -4px rgba(0,0,0,0.15), 0 20px 0 -10px #eee, 0 20px 1px -9px rgba(0, 0, 0, 0.15)',
-	'padding': '20px', 'background-color': 'white'});
-		}
-		
+		$('.active figure').css({'box-shadow': '0 1px 1px rgba(0,0,0,0.15)'});
 	}
 }
 
