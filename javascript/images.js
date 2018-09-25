fac42908a216971586044a61236a466bc91d1a72
Marina <musicbymarina@gmail.com>
2018-09-26 01:22:44 +0200

    orientate landscape modal

--- javascript/images.js
+++ javascript/images.js
@@ -1,305 +1,477 @@
-const data = [
-{
-		"name": "Daniel Avery",
-		"type": "electro",
-		"venue": "Peacock Society",
-		"photosPath": [
-		"https://scontent-cdg2-1.xx.fbcdn.net/v/t31.0-8/12694544_1266530113380843_4985876799365095477_o.jpg?_nc_cat=0&oh=6f17468d494366a774cc6b9a22ae856b&oe=5C38C229",
-		"https://scontent-cdg2-1.xx.fbcdn.net/v/t31.0-8/12513567_1266530373380817_5352358043785360024_o.jpg?_nc_cat=0&oh=6ba152bdd02c55c4aed6edde07a116e6&oe=5C29989F"
-		]
-	},
-	{
-		"name": "The Black Lips",
-		"type": "rock",
-		"venue": "La Villette",
-		"photosPath": [
-		"https://scontent-cdg2-1.xx.fbcdn.net/v/t31.0-8/23632116_1934358369931344_7426729873053806612_o.jpg?_nc_cat=0&oh=d4a0d3b0d6f8d877ed18660d1a1a1591&oe=5C34A018",
-		"https://scontent-cdg2-1.xx.fbcdn.net/v/t31.0-8/23509258_1934358429931338_2292833924781772992_o.jpg?_nc_cat=0&oh=8367aa7bc48d97abc450e2989704f2e7&oe=5C2DB3F5",
-		"https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/23517831_1934359216597926_5576395402958645362_n.jpg?_nc_cat=0&oh=74e9c6e16408ffe3d39ddb645a86918a&oe=5C1CF98E"
-		]
-	},
-	{
-		"name": "The Jesus and Mary Chain",
-		"type": "rock",
-		"venue": "La Route du Rock 2017",
-		"photosPath": [
-		"https://scontent-cdg2-1.xx.fbcdn.net/v/t31.0-8/21054900_1848116595222189_3410498097011115445_o.jpg?_nc_cat=0&oh=ce3ef5832bc1d6f57ccf9ed94bda2d33&oe=5C2DD375",
-		"https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/20992966_1848116818555500_4471541284690943894_n.jpg?_nc_cat=0&oh=610a4ddc733871437864844cb5c131d2&oe=5C1F6471"
-	]
-}
-];
-
-/**
-Je crÃ©e une nouvelle liste pour rÃ©cupÃ©rer les photos des djs seulement
-**/
-const electroList = data.filter((photo) => {
-	return photo.type === 'electro';
-})
-
-/**
-Je crÃ©e une nouvelle liste pour rÃ©cupÃ©rer les photos des groupes de rock seulement
-**/
-const rockList = data.filter((photo) => {
-	return photo.type === 'rock';
-})
-
-/*
-Je cree un titre
+/*COLORS:
+- dark : black (background body + menu hover)
+- grey: #57595B (color for buttons)
+- light : white (hover menu background + color)
 */
-const creerTitre = (liste) =>{
-	switch(liste) {
-		case 'electroList':
-		$('#titre').html('Photographer for DJs');
-		break;
-		
-		case 'rockList':
-		$('#titre').html('Photographer for Rock bands');
-		break;
-		
-		default:
-		$('#titre').html('Photographer');
-	}
+
+* {
+	font-family: Roboto, sans-serif;
+	color: white;
 }
 
-
-/**
-Je montre les photos de la liste choisie dans le slider et le thumbnail en mÃªme temps
-paramÃ¨tre: la liste
-**/
-const slider = $('#slider');
-const thumbnail = $('#thumbnail');
-
-function fetchPhotos(liste){
-if(liste) {
-	liste.map(artiste=> {
-		artiste.photosPath.map((photo)=>{
-			const content = 
-			`<li>
-				<figure style='box-shadow: 0 1px 1px rgba(0,0,0,0.15), 0 10px 0 -5px #eee, 0 10px 1px -4px rgba(0,0,0,0.15), 0 20px 0 -10px #eee, 0 20px 1px -9px rgba(0, 0, 0, 0.15); padding: 20px; background-color: white'>
-					<img src='${photo}' alt='${artiste.name} at ${artiste.venue}' style='border: solid 1px #e8e6e6'>
-					<figcaption>${artiste.name} at ${artiste.venue}</figcaption>
-				</figure>
-			</li>`;
-		slider.prepend(content);
-		thumbnail.prepend(content);
-		});
-	});
-} else {
-	console.warn('check si une liste apparait dans fetchPhotos');
-}
-// je ne montre que la premiÃ¨re image dans le slider, je cache les autres
-const firstLiSlider = $('#slider li:first');
-const autresLiSlider = $('#slider li:first ~li');
-firstLiSlider.addClass('active');
-autresLiSlider.addClass('hidden');
-
-// j'arrange l'image du slider pour qu'elle soit au milieu
-$('#slider').css({'margin': 'auto', 'list-style-type' : 'none'});
-
-// je fais un effet avec le figcaption
-$('#slider figcaption').fadeIn('slow');
-
-// j'arrange la liste de mon thumbnail
-$('#thumbnail').css({'margin': 'auto'});
-
-// je calcule le width de chaque li selon la largeur de mon ul
-if(thumbnail !== undefined) {
-	console.log(thumbnail);
+body {
+	background-color: black;
+	color: white;
 }
 
-$('#thumbnail li').css({'margin': '5px', 'width': 'auto'});
-$('#thumbnail figure').css({'margin': '1em'});
-	
-//je cache le figcaption du thumbnail
-$('#thumbnail figcaption').hide();
-$('#thumbnail figure').css({'box-shadow': 'none', 'padding':'0', 'background-color':'black'});
+/*MENU*/
 
-// Je crÃ©e mon titre
-creerTitre(liste);
+header {
+	display: flex;
+	flex-direction:row;
+	width: 100%;
+	margin: 0 auto;
 }
 
-/* J'ajoute un border blanc dans le thumbnail qui correspond a la photo du slider
-A chaque fois qu'une photo sera vue dans le slider, chaque photo vue aura ce border
-Et je cache le figcaption dedans (pas esthetique)
-(comme ca on sait celles qu'on a pas vu)
-*/
-const displayThumbnail = () =>{
-	// je rÃ©cupÃ¨re l'index du li du slider qui est actif
-	const indexSlider = $('#slider').find('li.active').index();
-	
-	// je rÃ©cupere le li du thumbnail qui a le meme index que le li du slider actif
-	const thumbnailToDisplay = $('#thumbnail li')[indexSlider];
-	thumbnailToDisplay.style.border = 'solid 5px white';
+header h3{
+	text-transform: uppercase;
+	display: none;
+}
+
+#logo{
+	width: 20%;
+	display: flex;
+	flex-direction: column;
+	margin: 0 1em;
+}
+
+#logo h1{
+	font-family: 'Notable', sans-serif;
+	color: white;
+	text-align: center;
+}
+
+#burger{
+	width: 40%;
+	display: none;
+	flex-direction: column;
+	margin: 1em 0;
+	align-content: flex-start;
+	justify-content: flex-start;
+}
+
+#burger > div {
+	width: 50%;
+	height: 10px;
+	margin: 5px 10%;
+	background-color: #E2DCD9;
 	
 }
 
+h1, h2 {
+	text-align: left;
+}
 
-// Creer une promise pour ensuite fetcher par dÃ©faut toutes mes photos dans le slider et le thumbnail
-const verifFetch = new Promise((resolve, reject) =>{
+h1, li {
+	text-transform: uppercase;
+}
 
-	if( slider.length > 0 && slider.length === thumbnail.length) {
-		resolve('Photos remplis dans le slider et le thumbnail');
-	} else {
-		reject('Pas bien rempli, recheck ta fonction fetchPhotos meuf ');
-	}
-})
+#titre{
+	display: inline-block;
+	width: 100%;
+	margin: 1em auto;
+}
 
-verifFetch.then(()=>{
-fetchPhotos(data);
-displayThumbnail();
-}).catch((error)=>{
-	console.warn('Check ton erreur dans la fonction fetchPhotos: ', error);
-})
+header nav{
+	display: block;
+	width: 70%;
+}
 
-/* utile pour cacher la photo actuelle du slider
- quand je clique dans le thumbnail ou sur les boutons next/previous
- */
-const cacherPhotoActuelleSlider = () => {
-		$('#slider').find('.active').addClass('hidden');
-		$('#slider').find('.active').removeClass('active');
+ul.parent-menu, ul.child-menu, ul#insert {
+	list-style-type: none;
+	font-size: 1em;
+	padding: 0;
+}
+
+ul.parent-menu {
+	padding: 1em;
+	display: flex;
+	flex-direction: row;
+}
+
+ul.parent-menu > li, ul.child-menu > li {
+	background-color: black;
+	border-bottom: solid 4px white;
+	text-align: center;
+}
+
+ul.parent-menu > li {
+	display: inline-block;
+	position: relative;
+	width: 18%;
+	padding: 0 1em 1em 1em;
+	margin-left: 1em;
+}
+
+ul.parent-menu > li a, 
+ul.child-menu > li a {
+	color: white;
+	text-decoration: none;
+	display: block;
+	padding: 1em;
+	height: 1em;
+}
+
+ul.child-menu {
+	display: none;
+	position: absolute;
+	margin: -4px;
+	top: 3em;
+	width: 100%;
+}
+
+ul.child-menu > li {
+	width: inherit;
+	margin-top: 30px;
+	padding-bottom: 1em;
 }
 
 
-/*
-FONCTION POUR NETTOYER MES ANCIENNES PHOTOS DU SLIDER ET THUMBNAIL
-Utile si je clique sur le bouton electro, rock et All
-*/
-const cleanMySlider = () =>{
-	slider.html('');
-	thumbnail.html('');
+ul.parent-menu > li:hover,
+ul.child-menu > li:hover {
+	background-color: white;
+	border-radius: 50px 20px;
+	border-left-color: grey;
+}
+
+.parent-menu > li a:hover,
+.child-menu > li a:hover{
+	color: black;
+	font-weight: bold;
+}
+
+
+/*EXTRAIT IMAGES*/
+
+main {
+	width: 100%;
+	padding: 0 2em;
+	box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
+}
+
+main h2{
+	width: 100%;
+	text-align: left;
+	margin: 1em auto;
+}
+
+section {
+	border: #E2DCD9;
+	width: 100%;
+}
+
+
+#buttons {
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+	margin: 1em auto;
+}
+
+button {
+	color: #57595B;
+	background-color: white;
+	text-transform: uppercase;
+	border: 5px solid black;
+	border-radius: 50px;
+	margin: 0 auto;
+	width: 30%;
+	height: 60px;
+	font-weight: bolder;
+	font-size: 1em;
+	width: 30%;
+	-webkit-box-shadow: -7px 7px 19px 7px rgba(0,0,0,0.75);
+	-moz-box-shadow: -7px 7px 19px 7px rgba(0,0,0,0.75);
+	box-shadow: -7px 7px 19px 7px rgba(0,0,0,0.75);
+}
+
+button:focus{
+	outline: 0;
+}
+
+button:active{
+	background-color: #57595B;
+	color: white;
+	-webkit-box-shadow: inset -7px 7px 19px 7px rgba(0,0,0,0.75);
+	-moz-box-shadow: inset -7px 7px 19px 7px rgba(0,0,0,0.75);
+	box-shadow: inset -7px 7px 19px 7px rgba(0,0,0,0.75);
+	transform: translateY(4px);
+}
+
+
+.slider {
+	display: flex;
+	margin: auto;
+	padding: 0;
+	width: 100%;
+}
+
+#slider > figure {
+	flex-wrap: wrap;
+	width: 100%;
+	display: none;
+	background-color: white;
+	box-shadow: 
+	/*Ombre de la 1ere couche:*/
+	0 1px 1px rgba(0,0,0,0.15),
+	/*2eme couche:*/
+	0 10px 0 -5px #eee,
+	/*Ombre de la 2eme couche:*/
+	0 10px 1px -4px rgba(0,0,0,0.15),
+	/*3eme couche:*/
+	0 20px 0 -10px #eee,
+	/*Ombre de la 3eme couche:*/
+	0 20px 1px -9px rgba(0, 0, 0, 0.15);
+	padding: 20px;
+}
+
+.hidden {
+	flex-wrap: wrap;
+	margin: auto;
+	display: none;
+	height: 0;
+	opacity: 0;
+	overflow: hidden;
+	transition: height 350ms ease-in-out, opacity 750ms ease-in-out;
+}
+
+#slider img {
+	max-width: 100%;
+	border: solid 1px #e8e6e6;
+}
+
+#slider figure.active {
+	display: flex;
+	height: auto;
+	margin: auto;
+	opacity: 1;
+	border: solid 1px #eee;
+	background-color: white;
+	box-shadow: 
+	/*Ombre de la 1ere couche:*/
+	0 1px 1px rgba(0,0,0,0.15),
+	/*2eme couche:*/
+	0 10px 0 -5px #eee,
+	/*Ombre de la 2eme couche:*/
+	0 10px 1px -4px rgba(0,0,0,0.15),
+	/*3eme couche:*/
+	0 20px 0 -10px #eee,
+	/*Ombre de la 3eme couche:*/
+	0 20px 1px -9px rgba(0, 0, 0, 0.15);
+	padding: 20px;
+}
+
+
+figcaption {
+	color: black;
+	font-family: 'Indie Flower', cursive;
+	text-align: center;
+	text-transform: capitalize;
+	font-size: 1.2em;
+	margin-top: 1em;
+}
+
+.slider ul {
+	margin: auto;
+	width: 100%;
+}
+
+/*BOUTONS PREVIOUS ET NEXT*/
+
+.slider-nav {
+	margin: 1em auto;
+}
+
+.slider-nav ul {
+	font-size: xx-large;
+	list-style: none;
+	margin: 0;
+	padding: 0;
+	display: flex;
+	-webkit-box-pack: center;
+	justify-content: center;
+}
+
+.slider-nav li {
+	font-size: xx-large;
+	-webkit-box-flex: 2;
+	flex: 2;
+	text-align: center;
+	display: -webkit-box;
+	display: flex;
+}
+
+.slider-nav .arrow {
+	flex: 0 0 15%;
+	font-size: xx-large;
+}
+
+.slider-nav a {
+	font-size: xx-large;
+	flex-basis: 100%;
+	display: -webkit-box;
+	display: flex;
+	-webkit-box-align: center;
+	align-items: center;
+}
+
+.slider-nav span {
+	font-size: 1.5em;
+	display: block;
+	width: 100%;
+	color: white;
 }
 
 /*
-SI JE CLIQUE SUR LE BOUTON NEXT DU SLIDER:
-1/ Je montre dans le thumbnail (border blanc) la photo sÃ©lectionnÃ©e (+1) sauf si c'est la derniÃ¨re photo du thumbnail
-2/ Je montre la photo correspondante dans le slider sauf si c'est la derniere toff du thumbnail
+THUMBNAIL
 */
-const showNextOrPreviousPicture = (whichOne) => {
-	// je rÃ©cupÃ¨re l'index de la photo actuellement active dans le slider
-	const indexPhotoActive = $('#slider').find('.active').index();
-	// Je prÃ©pare l'index de ma prochaine photo
-	const indexProchainePhoto = indexPhotoActive + whichOne;
-	// Je vÃ©rifie si c'est pas la derniere toff du thumbnail
-	const longueurSlider = $('#thumbnail li').length;
-	if(whichOne === 1 && indexProchainePhoto < longueurSlider) {
-		cacherPhotoActuelleSlider();
-		// je rÃ©cupÃ¨re la prochaine photo du slider et je la montre
-		const prochainePhoto = $('#slider li')[indexProchainePhoto];
-		prochainePhoto.classList.add('active');
-		prochainePhoto.classList.remove('hidden');
-		// je mets un border blanc sur la photo choisie dans le thumbnail
-		displayThumbnail();
-	} else if(whichOne === -1 && indexProchainePhoto >= 0) {
-		cacherPhotoActuelleSlider();
-		// je rÃ©cupÃ¨re la prochaine photo du slider et je la montre
-		const prochainePhoto = $('#slider li')[indexProchainePhoto];
-		prochainePhoto.classList.add('active');
-		prochainePhoto.classList.remove('hidden');
-		
-		// je mets un border blanc sur la photo choisie dans le thumbnail
-		displayThumbnail();
-	}
-	else {
-		console.log('ceci etait la derniere toff du slider');
-	}
+
+.thumbnail {
+	width: 100%;
 }
 
-$('.next span').on('click', () =>{
-	showNextOrPreviousPicture(+1);
-});
-
-$('.previous span').on('click', () =>{
-	showNextOrPreviousPicture(-1);
-});
-
-/*
-SI JE CLIQUE SUR LE BOUTON ELECTRO:
-Je montre les photos correspondantes a ma liste dans le slider et thumbnail
-*/
-$('#electro').click(()=>{
-	// je nettoie d'abord mon thumbnail et mon slider:
-	cleanMySlider();
-	// je montre les photos du slider et thumbnail correspondantes aux djs
-	fetchPhotos(electroList);
-	// je mets mon fameux border blanc sur le thumbnail
-	displayThumbnail();
-});
-
-/*
-SI JE CLIQUE SUR LE BOUTON ROCK:
-Je montre les photos correspondantes a ma liste dans le slider et thumbnail
-*/
-$('#rock').click(()=>{
-	// je nettoie d'abord mon thumbnail et mon slider:
-	cleanMySlider();
-	// je montre les photos du slider et thumbnail correspondantes aux rock bands
-	fetchPhotos(rockList);
-	// je mets mon fameux border blanc sur le thumbnail
-	displayThumbnail();
-});
-
-/*
-SI JE CLIQUE SUR LE BOUTON SHOW ALL:
-Je montre toutes les photos dans le slider et thumbnail
-*/
-$('#all').click(()=>{
-	// je nettoie d'abord mon thumbnail et mon slider:
-	cleanMySlider();
-	// je montre les photos du slider et thumbnail correspondantes aux djs
-	fetchPhotos(data);
-	// je mets mon fameux border blanc sur le thumbnail
-	displayThumbnail();
-});
-
-/*
-SI JE CLIQUE SUR LA PHOTO dans le thumbnail:
-1/ la photo correspondante doit avoir un border blanc
-2/ l'index de la photo doit correspondre a l'index de la photo du slider pour etre concordant
-parametre: event (pour rÃ©cupÃ©rer la zone choisie)
-*/
-
-const choosePhoto = (event) =>{
-	const photoChoisie = $(event.target);
-	console.log('la photo choisie est: ', photoChoisie);
-	if(photoChoisie.is('li')) {
-		// Je change le titre h2 par le figcaption de la photo
-		const titrePhotoChoisie = photoChoisie.find('figcaption').html();
-		$('#titre').html(`You chose to see ${titrePhotoChoisie}`);
-		// je trouve l'index de ma photo choisie dans le thumbnail
-		const indexPhotoChoisie = $('#thumbnail').find(photoChoisie).index();
-		// je cache la photo actuelle du slider
-		cacherPhotoActuelleSlider();
-		// je rÃ©cupÃ¨re l'index de la photo du slider qui correspond a la photo choisie et je la montre
-		const photoCorrespondante = $('#slider li')[indexPhotoChoisie];
-		photoCorrespondante.classList.add('active');
-		photoCorrespondante.classList.remove('hidden');
-		// je mets un border blanc sur la photo choisie dans le thumbnail
-		displayThumbnail();
-	}
+#thumbnail {
+	margin: auto;
+	height: auto;
+	display: flex;
+	flex-direction: row;
+	  align-items: center;
+  justify-content: center;
 }
 
-$('#thumbnail').click(choosePhoto);
+#thumbnail > li {
+	flex-wrap: wrap;
+	list-style-type: none;
+	margin: 5px;
+}
 
-/*SI JE CLIQUE SUR UNE PHOTO DU SLIDER EN MODE ACTIVE:
-- un modal s'ouvre
-- dans le modal (modal-content), j'ai l'image en grand écran
-- si je clique sur le close (.close), le modal se ferme
-*/
+#thumbnail figure {
+	margin: auto;
+}
 
-$('#slider img').on('click', (event) => {
-	const image = event.target;
-	console.log('the image is: ', image);
-	console.log('the src of the image is', image.src);
-	console.log('the alt of the image is', image.alt);
-	console.log(contenu);
-	const contenu = `
-	<img src=${image.src} alt=${image.alt}>
-	<figcaption>${image.alt}</figcaption>
-	`;
-	$('#modal-figure').prepend(contenu);
-	$('#myModal').show();
-});
+#thumbnail li img {
+	max-width: 100%;
+	width: auto;
+	max-height: 100%;
+	height: auto;
+	margin: auto;
+}
 
-$('.close').on('click', () =>{
-		$('#myModal').hide();
-});
+/*MODAL*/
+#mymodal{
+		display: none;
+		position: fixed;
+		z-index: 1;
+		padding-top: 5px;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		overflow: auto;
+		background-color: black;
+		background-color: rgba(0, 0, 0, 0.5);
+	}
+
+	.modal-content{
+		background-color: black;
+		margin: auto;
+		padding: 10px;
+		border: 1px solid white;
+		width: 95%;
+	}
+	
+	.close{
+		color: white;
+		font-weight: bolder;
+		font-size: 1.5em;
+		float: right;
+	}
+	
+	.close:hover,
+	.close:focus{
+		color: black;
+		text-decoration: none;
+		cursor: pointer;
+	}
+
+@media screen and (max-width: 500px) {
+	#logo{
+		width: 3%;
+	}
+	
+	#burger{
+		display: block;
+	}
+	
+	header nav{
+		visibility: hidden;
+		display: block;
+		width: 80%;
+	}
+	
+	ul.parent-menu{
+		display: flex;
+		flex-direction: column;
+		padding: 1em;
+		width: 80%;
+		box-sizing: content-box;
+	}
+	
+	ul.parent-menu > li{
+		display: flex;
+		flex-direction: column;
+		width: 70%;
+		padding: 0 1em 1em 1em;
+		margin-left: 1em;
+	}
+	
+	.child-menu{
+		flex-direction: column;
+		display: flex;
+	}
+	
+	main{
+		margin: 1em auto;
+	}
+	
+	#buttons{
+		flex-direction: column;
+		width: 90%;
+		margin: 0 auto;
+	}
+	
+	button{
+		width: 100%;
+		margin: 0 auto;
+		padding: 0;
+	}
+	
+	.active{
+		width: 90%;
+		padding: 0;
+		margin: 0;
+	}
+	
+	.active figure{
+		width: 100%;
+		margin: 1em 0;
+	}
+	
+	#thumbnail{
+		display: none;
+	}
+	
+	.force{
+		transform: rotate(-90deg);
+		transform-origin: left top;
+		width: 100vh;
+		height: 100vw;
+		overflow-x: hidden;
+		position: absolute;
+		top: 100%;
+		left: 0;
+	}
+	
+}
