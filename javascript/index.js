76bb1ec8d6c3e1fcb3d5733826c639f13837612d
Marina <musicbymarina@gmail.com>
2018-09-26 12:15:16 +0200

    test menu with mouseenter

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,7 @@
 /*
 When I click on Portfolio in the menu, I can see the submenu
 */
-$('#portfolio').on('click', ()=>{
+$('#portfolio').on('mouseenter', ()=>{
 
 	if($(window).width() < 700){
 		$('#portfolio-child').slideToggle();
@@ -24,7 +24,27 @@ $('#portfolio').on('click', ()=>{
 /*
 When I click on Works in the menu, I can see the submenu
 */
-$('#works').on('click', ()=>{
+$('#works').on('mouseenter', ()=>{
+	
+	if($(window).width() < 700){
+		$('#works-child').slideToggle();
+		$('#contact').toggle();
+	} else {
+		$('#portfolio-child').hide();
+		$('#works-child').slideToggle();
+	}
+	
+	$('#works').css({'background-color': 'white',
+	'border-radius': '50px 20px', 'border-left-color': 'grey'});
+	$('#work-a').css({'color': 'black', 'font-weight': 'bold'});
+	
+	$('#portfolio').css({'background-color': 'black',
+	'border-radius': '0', 'border-left-color': 'black'});
+	$('#portfolio-a').css({'color': 'white', 'font-weight': 'normal'});
+	
+});
+
+$('#works').on('mouseenter', ()=>{
 	
 	if($(window).width() < 700){
 		$('#works-child').slideToggle();
