40b7060acf49ed7cd58313cc246200de579062bc
Marina <musicbymarina@gmail.com>
2018-10-09 01:03:53 +0200

    add press

---
+++ javascript/press.js
@@ -0,0 +1,151 @@
+/* Si je clique sur un h3:
+- lui ajouter la classe active pour avoir le + qui se transforme en -
+- récuperer son index et l'utiliser dans l'article
+- mettre cet article en SlideDown et ajouter un toggleClass active(qui aura un display block)
+(De base les articles sont cachés)
+*/
+
+$('.press article').hide();
+$(this).on('click', (event) => {
+	const target = $(event.target);
+	const parents = target.parent();
+
+	if(target.is(".accordion")){
+		target.toggleClass('active');
+
+		const index = parents.find(target).index('h3');
+		const article = $('.press article');
+		if(article[index-1].style.display === 'block'){
+			article[index-1].style.display = 'none';
+		} else {
+			article[index-1].style.display = 'block';
+		}
+	}
+	})
+
+
+// Remplir l'article digital avec le nom du magazine, l'article et le lien correspondant
+const presse = "https://raw.githubusercontent.com/musicbymarina/test/master/javascript/press.json";
+
+// Je cree les titres 
+const addMagazines = (data) => {
+	let content = ``;
+	const digital = data.digital;
+	
+	digital.map((magazine)=> {
+		content = `<h4>${magazine.name}</h4>`;
+
+	$('#digital ul').prepend(content);
+	
+});
+	const paper = data.paper;
+	paper.map((magazine)=>{
+		content = `<h4>${magazine.name}</h4>`;
+
+		$('#print ul').prepend(content);
+	})
+	
+	const social = data.social;
+	social.map((images)=>{
+		content = `<li id='thumbnail'><h4>${images.artiste}</h4></li>`;
+
+		$('#social ul').prepend(content);
+	})
+	
+}
+
+// Je rentre les liens pour chaque titre digital
+const addDigitalLinks = () =>{
+	const titres = $('#digital h4');
+			$('#digital h4').each((index)=> {
+				const titre = titres[index];
+				fetch(presse).then(response=>response.json()).then((data)=>{
+					const listeMagazine = data.digital.filter((magazine) => magazine.name === titre.innerHTML);
+					listeMagazine.map((mag)=>{
+						const liens = mag.links;
+						liens.map((lien)=>{
+
+							const content = `<li><p>Subject: <a href=${lien.url} target='_blank'>${lien.artiste}</a></p></li>`;
+							titre.innerHTML = titre.innerHTML + `<ul>${content}</ul>`;
+						})
+					})
+				});
+			})
+}
+
+// Je rentre les liens pour chaque titre papier
+const addPrintLinks = () =>{
+		const titres = $('#print h4');
+			$('#print h4').each((index)=> {
+				const titre = titres[index];
+				fetch(presse).then(response=>response.json()).then((data)=>{
+					const listeMagazine = data.paper.filter((magazine) => magazine.name === titre.innerHTML);
+					listeMagazine.map((mag)=>{
+						const files = mag.images;
+						files.map((file)=>{
+							if(file.src.endsWith('.png')){
+								const content = `<li><p><img src=${file.src} alt=${mag.name} class='agrandir'> <figcaption>${file.artiste}</figcaption></p></li>`;
+								titre.innerHTML = titre.innerHTML + `<ul>${content}</ul>`;
+							} else if(file.src.endsWith('.pdf')){
+								const content = `<li><p>Subject: <a href=${file.src} target='_blank'>${file.artiste}</a></p></li>`;
+								titre.innerHTML = titre.innerHTML + `<ul>${content}</ul>`;
+							}
+							})
+						})
+					});
+				});
+}
+
+// Je rentre les liens pour chaque post instagram
+const addSocialLinks = () =>{
+			const titres = $('#social li');
+			$('#social li').each((index)=> {
+				const titre = titres[index];
+				fetch(presse).then(response=>response.json()).then((data)=>{
+					const socialPosts = data.social.filter((post) => post.artiste === $('#social h4')[index].innerHTML);
+					socialPosts.map((socialPost)=>{
+						const pictures = socialPost.pic;
+						pictures.map((pic)=>{
+								const content = `<figure><img src=${pic} alt=${socialPost.artiste} class='agrandir'> <figcaption>${socialPost.artiste}</figcaption></figure>`;
+								titre.innerHTML += content;
+							
+							})
+						})
+					});
+				});
+}
+
+// Je remplis la page
+fetch(presse).then((response)=>response.json()).then(addMagazines).then(()=>{
+	addDigitalLinks();
+	addPrintLinks();
+	addSocialLinks();
+});
+
+/*SI JE CLIQUE SUR UNE PHOTO DU SLIDER :
+- un modal s'ouvre
+- dans le modal (modal-content), j'ai l'image en grand écran
+- si je clique sur le close (.close), le modal se ferme
+*/
+const agrandirPhoto = () => {
+$('.agrandir img').on('click', (event) => {
+	console.log($(event.target));
+	$('.modal-content').html('');
+	$('html').addClass('force');
+	$('.modal-content').html(`<span class="close">X</span>`);
+	$(event.target).clone().appendTo('.modal-content');
+	$('.modal-content img').css({'width':'100%'});
+	$('#myModal').show();
+	
+
+	$('.close').on('click', () =>{
+		$('#myModal').hide();
+		$('html').removeClass('force');
+});
+});
+}
+
+// Tester si c'est parce que le DOM est loadé ou parce que j'ai déplacé les events listeners
+$(function() {
+    agrandirPhoto();
+  }); // fin function quand le DOM est loadé
